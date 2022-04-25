0bfa6156ba2f6c10d76d987ed4a990cb41c180da
-abinsinguza-dallen <dallenabinsinguza@gmail.com>
2022-04-25 07:45:17 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import calendar
+print (calendar.weekheader(3))
+print()
+
+print(calendar.firstweekday())
+print()
+print(calendar.month(2022,4))
+print(calendar.monthcalendar(2022,4))
+print(calendar.calendar(2022))
